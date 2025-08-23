50b4c5d8fa22a834579299e207ad594676670fb0
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-08-23 13:41:40 +0500

    crud operations

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from typing import Optional
-import asyncio
+from typing import List
 
 app = FastAPI()
-
-# @app.get("/hello/{name}")
-# def say_hello(name: str, age: int = 10):
-#     return {"message": f"Hello {name}, you are {age} years old"}
-
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
-    bio: Optional[str] = "No bio provided!"
-
-@app.post("/user/")
-def create_user(user: User):
-    return {"message": f"User {user.name} with email {user.email} has been created successfully"}
-
-@app.get("/books/{book_id}")
-def get_books(book_id: int, book_title: str):
-    return {"book_id": book_id, "book_title": book_title}
 
 class Book(BaseModel):
     title: str
     author: str
     page: int = Field(..., gt=100)
 
+books: List[Book] = []
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my bookstore!"}
+
+@app.get("/books/")
+def get_all_books():
+    return books
+
+@app.get("/books/{book_id}")
+def get_books(book_id: int, book_title: str):
+    return {"book_id": book_id, "book_title": book_title}
+
 @app.post("/books/")
 def create_book(book: Book):
-    return {"message": f"Book {book.title} has been created)"}
+    books.append(book)
+    return {"message": f"Book {book.title} has been created"}
 
-@app.get("/wait/{seconds}")
-async def wait_for(seconds: int):
-    await asyncio.sleep(seconds)
-    return {"message": f"Slept for {seconds} seconds"}
+@app.put("/books/{book_id}")
+def update_book(book_id: int, updated_book: Book):
+    for index, book in enumerate(books):
+        books[index] = updated_book
+        return {"message": f"Book {book_id} has been updated"}
+    return {"error": f"Book {book_id} not found"}
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    books.pop(book_id)
+    return {"message": f"Book {book_id} has been deleted"}
+
